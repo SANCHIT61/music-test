42e47ac9b87799c456b77a4288c8b7b653bffaaa
SANCHIT61 <SANCHITNATH@.github.com>
2020-06-18 00:58:42 +0530

    Update index.ts
    The reason to do the second commit are :-
    1. Removed "your comments" for which the issue was resolved
    2. Replace the "repeated code" with another variable
    3. Updated the comment in "setTimeout" for proper understanding

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -8,28 +8,29 @@ function playMusic() {
     let bpm = 240;
 
     notes.forEach
-    (function playNext(note, index, notes)  // REVIEW - Weird choice - creating an anonymous function, then assigning it to a constant. Better to create a function declaration.
-    {   // index and notes are not reqd in the parameters since i only used "note" but i still added it
+    (function playNext(note, index, notes)  
+    {   // index and notes are not reqd in the parameters since I only used "note" but I still added it
         
         const audio = new Audio(); // creating audio // REVIEW - Unclear instantiation - better to call the constructor with a source URL
+        //audio.src used below does the same as -> [//let URL = document.getElementById("id").src; // const audio = new Audio(URL);]
+        
+        let pitchOctave = note.pitch + note.octave.toString(); // Repeated code added here
 
-        if(note.accidental) // Find the source which has accidental // REVIEW - Unnecessarily complex condition. There are cleaner ways to check for a truthy value
+        if(note.accidental) // Find the source which has accidental 
         {
-            audio.src = note.pitch + note.octave.toString() + " " + note.accidental + ".mp3";   // REVIEW - This is more readable with a couple of intermediate variables and template strings
+            audio.src = pitchOctave + " " + note.accidental + ".mp3";  
         }
         else
         {
-            audio.src = note.pitch + note.octave.toString() + ".mp3";  // REVIEW - Repeated code. Factor out some of this to make it readable
-                                                                                // Plus, you are mixing single quotes and double quotes in the same line. It's recommended to choose one set and stick to it for the entire project
+            audio.src = pitchOctave + ".mp3";  
         }
 
         audio.play();
 
-        setTimeout( () => // Pause the audio after (notes[i].beats * 250)ms // REVIEW - Fat arrow function might be a better fit here, since the function is anonymous anyway
+        setTimeout( () => // Pause the audio after (notes[i].beats * 60 * 1000 / bpm)ms 
         {
             audio.pause();
-        }, (note.beats) * ((60 * 1000) / bpm )); // BEATS_PER_MINUTE = 240, X BEATS = Y milliseconds  // REVIEW - Calculation logic is hard to read. Can take more steps, with intermediate variables. 
-                                    // Plus, there are no spaces between operators - inconsistent with previous style, which used spaces between them.
+        }, (note.beats) * ((60 * 1000) / bpm )); // BEATS_PER_MINUTE = 240, X BEATS = Y milliseconds  
     });
 }
 
