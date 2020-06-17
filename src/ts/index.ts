1ae7d1e49478f33a27dde370c0d2a17bae251624
SANCHIT61 <SANCHITNATH@.github.com>
2020-06-17 15:12:43 +0530

    Update index.ts
    Updated this file according to the mentioned comments.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,39 +4,34 @@ function playMusic() {
     const notes = notesToPlayInOrder;
     
     // TODO Play these notes one after the other at the pitch and rhythm given in each note
+    
+    let bpm = 240;
 
-    var i = -1;
-    const playNext = () => 
-    {
-        i++;
-        if(notes[i])
+    notes.forEach
+    (function playNext(note, index, notes)  // REVIEW - Weird choice - creating an anonymous function, then assigning it to a constant. Better to create a function declaration.
+    {   // index and notes are not reqd in the parameters since i only used "note" but i still added it
+        
+        const audio = new Audio(); // creating audio // REVIEW - Unclear instantiation - better to call the constructor with a source URL
+
+        if(note.accidental) // Find the source which has accidental // REVIEW - Unnecessarily complex condition. There are cleaner ways to check for a truthy value
         {
-            const audio = new Audio(); // creating audio for each object present in the array
-
-            if(notes[i].accidental || notes[i].accidental!= null) // Find the source which has accidental
-            {
-                audio.src = notes[i].pitch + notes[i].octave.toString() + " " + notes[i].accidental + ".mp3";
-            }
-            else
-            {
-                audio.src = notes[i].pitch + notes[i].octave.toString() + ".mp3";
-            }
-
-            audio.currentTime = 0; // Play the new audio from the start while keeping the current time as 0
-            audio.play();
-
-            setTimeout(function() // Pause the audio after (notes[i].beats * 250) milli-seconds
-            {
-                audio.pause();
-            }, (notes[i].beats)*250); // BEATS_PER_MINUTE = 240, X BEATS = Y milliseconds 
-
-            audio.addEventListener("pause", function() // eventlistener to call the playNext() once the audio is paused
-            {
-                return playNext(); // recursion
-            });
+            audio.src = note.pitch + note.octave.toString() + " " + note.accidental + ".mp3";   // REVIEW - This is more readable with a couple of intermediate variables and template strings
         }
-    }
-    playNext(); // Method/function to play audio one after another
+        else
+        {
+            audio.src = note.pitch + note.octave.toString() + ".mp3";  // REVIEW - Repeated code. Factor out some of this to make it readable
+                                                                                // Plus, you are mixing single quotes and double quotes in the same line. It's recommended to choose one set and stick to it for the entire project
+        }
+
+        audio.play();
+
+        setTimeout( () => // Pause the audio after (notes[i].beats * 250)ms // REVIEW - Fat arrow function might be a better fit here, since the function is anonymous anyway
+        {
+            audio.pause();
+        }, (note.beats) * ((60 * 1000) / bpm )); // BEATS_PER_MINUTE = 240, X BEATS = Y milliseconds  // REVIEW - Calculation logic is hard to read. Can take more steps, with intermediate variables. 
+                                    // Plus, there are no spaces between operators - inconsistent with previous style, which used spaces between them.
+    });
 }
 
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
+
