994c552a82e1ffd5f6c02d58bf21c800276606c1
SANCHIT61 <SANCHITNATH@.github.com>
2020-12-13 17:11:33 +0530

    Update index.ts
    Alternative to for loop

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,28 +7,34 @@ function playMusic() {
     let srcNodeList = document.getElementsByClassName("audio-elements-holder")[0].querySelectorAll("audio[src*='./src/assets']");
     const length = srcNodeList.length;
     const timeOut = (length * ((60 * 1000) / bpm)); // BEATS_PER_MINUTE = 240, X BEATS = Y milliseconds
+    let musicArray = Array.from(srcNodeList); // Created array out of NodeList<E>
+    let playPromise;
+    let i = -1;
     let audio = new Audio();
     audio.load();
-    
-    (function () {
-    for (let i = 0; i < length; i++) {
-            let sourceUrl = srcNodeList[i].getAttribute('src')
-            if (sourceUrl) {
-                audio.src = sourceUrl
-            }
-            let playPromise = audio.play();
-            if ((playPromise !== undefined) || (playPromise !== null)) {  // same as - [if (playPromise)]
-                playPromise
-                .then(() => {
-                    setTimeout(() => // Pause the audio after -> (((total number of beats) * 60 * 1000) / (bpm)) milliseconds 
-                    {
-                       audio.pause();
-                    }, timeOut)  
-                })
-                .catch(e => {
-                    console.log(`Error is -> ${e}`); // Template literal
-                })
-            }
+
+    (function play() {
+        i+=1;
+
+        let sourceUrl = musicArray[i].getAttribute('src');
+        if (sourceUrl) {
+            audio.src = sourceUrl
+        }
+        playPromise = audio.play();
+        if ((playPromise !== undefined) || (playPromise !== null)) {  // same as - [if (playPromise)]
+            console.log(`Started playing -> ${sourceUrl} -> ${i}`);
+            playPromise
+            .then(() => {
+                setTimeout(() => // Pause the audio after -> (((total number of beats) * 60 * 1000) / (bpm)) milliseconds 
+                {
+                    audio.pause();
+                    console.log(`Paused?? -> ${i}`);
+                    play();
+                }, timeOut)  
+            })
+            .catch(e => {
+                console.log(`${i} Error is -> ${e}`);
+            })
         }
     })
     (); // Self invoking function 
